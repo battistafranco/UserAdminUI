bfcecc91ed848e81fb60a65a95a9fc916b25425e
Franco Battista <fbattista@primary.com.ar>
2019-07-05 10:57:33 -0300

    ng upgrade

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { CommonModule  } from "@angular/common";
 
 import { UsersRoutingModule } from "./users-routing.module";
 import { UsersPageComponent } from "./pages/users-page/users-page.component";
 import { UsersFormComponent } from "./components/users-form/users-form.component";
 import { UsersListComponent } from "./components/users-list/users-list.component";
 import { SharedComponentsModule } from "src/app/shared/components/shared-components.module";
-//import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UsersService } from "./services/users.service";
 import { TranslatePipe } from "src/app/pipes/translate.pipe";
 import { PesonalDataFormComponent } from './components/pesonal-data-form/pesonal-data-form.component';
@@ -17,6 +17,8 @@ import { PesonalDataFormComponent } from './components/pesonal-data-form/pesonal
     CommonModule,
     UsersRoutingModule,
     SharedComponentsModule,
+    FormsModule,
+    ReactiveFormsModule
   
   ],
   exports: [UsersPageComponent, UsersFormComponent, UsersListComponent],  
